30aee080f49b39f3ef64f6617dad25c5768104d4
fchamza7khan <123562423+fchamza7khan@users.noreply.github.com>
2023-02-09 15:51:58 +0500

    initial commit

---
+++ Demo SwiftUI/Demo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_SwiftUIApp.swift
+//  Demo SwiftUI
+//
+//  Created by Hamza Muhammad on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Demo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
